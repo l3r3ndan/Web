6fd2b4f96ce1acd57110df0de950761f91fe4064
l3r3ndan <brendanobrien_7@hotmail.com>
2013-07-25 23:37:50 +1000

    commit 7
    added layout links with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+  root :to => "pages#home"
+  
+  get '/contact', :to => 'pages#contact'
+  get '/about', :to => 'pages#about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
